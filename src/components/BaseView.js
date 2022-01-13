6cf197228bf2bb21902c297b1fc42fd2feda125e
noahNoh <cananohyoo@gmail.com>
2022-01-13 01:48:02 -0800

    modified Copyright

--- src/components/BaseView.js
+++ src/components/BaseView.js
@@ -90,7 +90,7 @@ const BaseView = () => {
             </section>
 
             <footer className="bg-light py-5" >
-                <div className="container px-4 px-lg-5"><div className="small text-center text-muted">Copyright &copy; 2021 - Company Name</div></div>
+                <div className="container px-4 px-lg-5"><div className="small text-center text-muted">Copyright &copy; 2022 - Mini Crypto</div></div>
             </footer >
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
