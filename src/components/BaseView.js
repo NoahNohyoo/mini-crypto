c7784207102bea2a51b7fb662ba93d8f54c986dc
noahNoh <cananohyoo@gmail.com>
2022-01-13 01:30:49 -0800

    forTest

---
+++ src/components/BaseView.js
@@ -0,0 +1,102 @@
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
+
+const BaseView = () => {
+    window.addEventListener('DOMContentLoaded', event => {
+
+        // Navbar shrink function
+        const navbarShrink = function () {
+            const navbarCollapsible = document.body.querySelector('#mainNav');
+            if (!navbarCollapsible) {
+                return;
+            }
+            if (window.scrollY === 0) {
+                navbarCollapsible.classList.remove('navbar-shrink')
+            } else {
+                navbarCollapsible.classList.add('navbar-shrink')
+            }
+
+        };
+
+        // Shrink the navbar 
+        navbarShrink();
+
+        // Shrink the navbar when page is scrolled
+        document.addEventListener('scroll', navbarShrink);
+
+        // Activate Bootstrap scrollspy on the main nav element
+        const mainNav = document.body.querySelector('#mainNav');
+        if (mainNav) {
+            new bootstrap.ScrollSpy(document.body, {
+                target: '#mainNav',
+                offset: 74,
+            });
+        };
+
+        // Collapse responsive navbar when toggler is visible
+        const navbarToggler = document.body.querySelector('.navbar-toggler');
+        const responsiveNavItems = [].slice.call(
+            document.querySelectorAll('#navbarResponsive .nav-link')
+        );
+        responsiveNavItems.map(function (responsiveNavItem) {
+            responsiveNavItem.addEventListener('click', () => {
+                if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                    navbarToggler.click();
+                }
+            });
+        });
+
+    });
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+                <div className="container px-4 px-lg-5">
+                    <a className="navbar-brand" href="">Mini Crypto</a>
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                            <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <header className="masthead">
+                <div className="container px-4 px-lg-5 h-100">
+                    <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                        <div className="col-lg-8 align-self-end">
+                            <h1 className="text-white font-weight-bold">New portfolio in 2022</h1>
+                            <hr className="divider" />
+                        </div>
+                        <div className="col-lg-8 align-self-baseline">
+                            <p className="text-white-75 mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
+                            <a className="btn btn-primary btn-xl" href="#about">Find Out More</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <section className="page-section bg-primary" id="about">
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-lg-8 text-center">
+                            <h2 className="text-white mt-0">We've got what you need!</h2>
+                            <hr className="divider divider-light" />
+                            <p className="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
+                            <a className="btn btn-light btn-xl" href="#services">Get Started!</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <footer className="bg-light py-5" >
+                <div className="container px-4 px-lg-5"><div className="small text-center text-muted">Copyright &copy; 2021 - Company Name</div></div>
+            </footer >
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+            <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+        </div>
+    )
+}
+
+export default BaseView;
